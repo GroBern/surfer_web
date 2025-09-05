2d0a677368253d60ebffc07c792ab1bb2067abc4
it21110016 <it21110016@my.sliit.lk>
2025-09-05 22:48:18 +0530

    minor updates

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -408,7 +408,7 @@ const SingleBlog = () => {
     <div>
       {/* Hero Section */}
       <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'
-        style={{ backgroundImage: `url(${t("blog.headerImage")})` }} id='Header'>
+        style={{ backgroundImage: `url(${t("/f3.jpg")})` }} id='Header'>
         <Navbar />
         <div className='container text-center mx-auto py-4 px-4 sm:px-6 md:px-20 lg:px-32 text-white'>
           <h2 className='font-[montserrat] text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-[100px] inline-block max-w-full sm:max-w-3xl font-bold pt-18 mt-16 sm:mt-8 md:-mt-2'>
